45f42416b3e7d9483c089b92d15bbf794a7540db
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-06-09 17:04:18 -0300

    feat: adjusting css to mobile

--- resources/js/components/atoms/FmMenuItem.vue
+++ resources/js/components/atoms/FmMenuItem.vue
@@ -5,9 +5,7 @@
   >
     <span :class="[
         'fm-menu-item__text',
-        'fm-text-color-1',
-        'fm-font-size-16',
-        isExact ? 'fm-font-weight-bold' : 'fm-font-weight-light'
+        { 'fm-menu-item__text--exact': isExact }
       ]"
     >
       {{ label }}
@@ -46,17 +44,3 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
-
-.fm-menu-item {
-  height: 100%;
-
-  &__text {
-    width: 100%;
-    min-width: 200px;
-
-    align-self: center;
-    text-align: center;
-  }
-}
-</style>
