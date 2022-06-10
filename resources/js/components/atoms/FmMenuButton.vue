57ec2dbe1e9ad04bac07ff2b6583fc7825905a62
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-06-09 21:05:38 -0300

    feat: removed comments

--- resources/js/components/atoms/FmMenuButton.vue
+++ resources/js/components/atoms/FmMenuButton.vue
@@ -2,7 +2,7 @@
   <div
     :class="[
       'fm-menu-button',
-      { 'fm-menu-button__active' : isShowMenu }
+      { 'fm-menu-button__active': isShowMenu }
     ]"
     @click="handleClick"
   >
@@ -33,15 +33,9 @@ export default {
 .fm-menu-button {
   height: 20px;
   display: flex;
-  /* border-radius: 3px; */
-  /* border: 1px solid  #8C8C8C; */
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
-  /* padding: 5px; */
-  // animation: show-menu 3s ease-in 3s forwards;
-  // transform: translate3d(100vw, 0, 0);
-  /* box-shadow: 0 0 3px 1px rgba(255, 255, 255, 0.2); */
 
   &,
   span {
