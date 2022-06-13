2c49389455a6304b470d19027d4e040fe6c3d079
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-06-13 12:31:52 -0300

    feat: created pokemon list page

---
+++ resources/js/modules/pokedex/views/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pokedex-list">
+    <pokemon-card />
+  </div>
+</template>
+
+<script>
+import PokemonCard from '../partials/List/PokemonCard.vue'
+
+export default {
+  name: 'pokedex-list',
+  components: {
+    PokemonCard
+  }
+}
+</script>
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 
+@section('class', 'pokedex-list-background')
+
 @section('content')
 <div class="container">
   <div class="list">
     Listagem
+    <pokedex-list />
   </div>
 </div>
 @endsection
