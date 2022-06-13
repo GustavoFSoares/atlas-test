6279829730075ab5f91c11d53e5360f92f9ffd3b
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-06-13 16:02:13 -0300

    feat: added props to pokemon card

--- resources/js/modules/pokedex/partials/List/PokemonCard.vue
+++ resources/js/modules/pokedex/partials/List/PokemonCard.vue
@@ -4,23 +4,31 @@
       <div class="img" />
     </div>
 
-    <div class="pokemon-card__container">
+    <div
+      class="pokemon-card__container"
+      :class="`fm-pokemon-color__background--${mainPokemonType}`"
+    >
       <div class="header">
-        <span class="header__identifier">
-          #001
-        </span>
+        <span class="header__identifier">#{{ identifier }}</span>
       </div>
 
       <div class="data__card">
         <div class="data__card__container">
           <div class="data__card__container__content">
             <div class="types">
-              <div class="types__item types__item--grass">G</div>
-              <div class="types__item types__item--poison">P</div>
+              <img
+                v-for="type in types"
+                :key="type"
+                class="types__item types__item"
+                :src="`/img/pokemon-types/${type}.png`"
+              />
             </div>
 
             <div class="name">
-              <h4 class="name__text">bulbasaur</h4>
+              <h4
+                class="name__text"
+                :class="`fm-pokemon-color__text--${mainPokemonType}`"
+              >{{ name }}</h4>
             </div>
           </div>
         </div>
@@ -32,5 +40,29 @@
 <script>
 export default {
   name: 'pokemon-card',
+  props: {
+    identifier: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    types: {
+      type: Array,
+      required: true
+    },
+    image: {
+      type: String
+    }
+  },
+  computed: {
+    mainPokemonType() {
+      const [mainType] = this.types
+
+      return mainType
+    }
+  }
 }
 </script>
