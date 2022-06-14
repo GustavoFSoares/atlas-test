3082e413874aa8e25be7e5c623753c1032c4e986
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-06-14 13:33:33 -0300

    refactor: moving scss from FmMenu to inside of component

--- resources/js/components/organisms/FmMenu.vue
+++ resources/js/components/organisms/FmMenu.vue
@@ -60,3 +60,121 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "@/sass/_flex.scss";
+@import "@/sass/_colors.scss";
+@import "@/sass/_mixins.scss";
+@import "@/sass/_container.scss";
+
+@keyframes show-menu {
+  0% {
+    transform: translate3d(50px, 0, 0);
+    opacity: 0;
+  }
+  80% {
+    opacity: 0;
+  }
+  100% {
+    transform: translate3d( 0, 0, 0);
+  }
+}
+
+.menu {
+  @extend .pokeatlas-container;
+
+  @extend .flex-space-between;
+  @extend .flex-center-center;
+
+  position: relative;
+  width: auto;
+  height: 60px;
+
+  margin: 0 auto;
+  padding: 0 20px;
+
+  @include media('tablet', 'min') {
+    padding: 20px 0;
+  }
+
+  &__logo, &__button {
+    z-index: 2;
+  }
+
+  &__button {
+    @include media('tablet', 'min') {
+      display: none !important;
+    }
+  }
+
+  &__items {
+    height: 35px;
+
+    display: flex;
+    justify-content: space-between;
+
+    @include media('mobile', 'max') {
+      top: 0;
+      left: 0;
+
+      position: fixed;
+
+      width: 100vw;
+      height: 100%;
+
+      z-index: 1;
+      background-color: #2C2C2D;
+
+      transform: translate3d(100vw, 0, 0);
+      transition: ease 1s;
+
+      .menu--active & {
+        transform: translate3d(0, 0, 0);
+      }
+    }
+
+    .menu__items-container {
+      display: flex;
+      gap: 10px;
+
+      @include media('mobile', 'max') {
+        margin-top: 100px;
+
+        width: 100%;
+        gap: 30px;
+        flex-direction: column;
+      }
+
+      .menu__item {
+        display: flex;
+
+        gap: 10px;
+        flex-direction: row;
+
+        @include media('mobile', 'max') {
+          gap: 30px;
+          flex-direction: column;
+
+          width: 100%;
+          padding: 0 30px;
+        }
+
+        &:nth-child(odd) {
+          &:after {
+            content: '';
+            border: 1px solid map-get($colors, 1);
+
+            height: 33px;
+
+            @include media('mobile', 'max') {
+              height: initial;
+              margin: 0 50px;
+              opacity: 0.5;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
