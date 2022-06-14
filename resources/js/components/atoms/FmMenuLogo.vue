8a38c21807b48a25dd40e5e1b52eed5101942d36
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-06-14 09:21:10 -0300

    fix: fmMenuLogo BEM structure

--- resources/js/components/atoms/FmMenuLogo.vue
+++ resources/js/components/atoms/FmMenuLogo.vue
@@ -3,7 +3,7 @@
     <h2 class="fm-menu-logo__title">PokeAtlas</h2>
 
     <div class="fm-menu-logo__decorator">
-      <div class="fm-menu-logo__decorator--content" />
+      <div class="fm-menu-logo__decorator__content" />
     </div>
   </div>
 </template>
