d111717d9084fc754c5e2686431bc9e5c53b06f9
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-06-14 23:19:07 -0300

    feat: create pokemon store

---
+++ resources/js/store/modules/PokemonApi/actions.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+export default {
+  async getPokemons({ dispatch, getters, commit }, { limit, search }) {
+    const { data } = await axios.get(`${getters.apiUrl}/pokemon`, {
+      params: {
+        limit,
+        search
+      }
+    })
+
+    const mappedPokemons = data.results.map(pokemon => {
+      let [pokemonId] = pokemon.url.match(/\d+\/$/g)
+      pokemonId = pokemonId.replace('/', '')
+
+      return {
+        id: pokemonId,
+        name: pokemon.name,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`,
+        types: []
+      };
+    });
+
+    commit("STORE_POKEMONS", mappedPokemons);
+
+    mappedPokemons.forEach(pokemon => {
+      dispatch('getPokemonData', pokemon)
+    });
+
+  },
+  async getPokemonData({ getters, commit }, { id }) {
+    const { data } = await axios.get(`${getters.apiUrl}/pokemon/${id}`);
+
+    const pokemonData = {
+      id: data.id,
+      types: data.types.map(pokemonType => pokemonType.type.name)
+    };
+    commit("STORE_SPECIFIC_POKEMONS", pokemonData);
+  }
+}
